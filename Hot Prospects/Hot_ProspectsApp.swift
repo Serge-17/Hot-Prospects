bfb7dafc89fc96728d90d53cbf0710d148a68634
Sergey Eliseev <sergei.eliseev17@icloud.com>
2026-01-21 13:51:16 +0300

    Initial Commit

---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Serge Eliseev on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
